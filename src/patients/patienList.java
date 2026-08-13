626eade031a8a7f4954f8d2ee427228d22b6bc1c
iva <ivallen84@gmail.com>
2026-08-13 16:28:03 -0600

    PatientList
    Se modifican los metodos de patientList

--- src/patients/patienList.java
+++ src/patients/patienList.java
@@ -4,14 +4,55 @@
  */
 package patients;
 
+import clinic.KeyDynamicsList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  *
  * @author PC
  */
 public class patienList implements KeyDynamicsList<Patient,String> {
-    private HashMap<String, Patient> patiens;
+    private HashMap<String, Patient> patients;
+
+   //preguntar por el contructor
+
+    @Override
+    public Patient get(String id) {
+        if(!patients.containsKey(id)) return null;
+     return patients.get(id);  
+    }
+
+    public boolean remove(Patient id) {
+        return patients.remove(id)!=null;
+    }
+
+    @Override
+    public boolean add(Patient item) {
+        if(patients.containsKey(item.getId())) return false;
+        return patients.put(item.getId(), item)== null;
+    
+    }
+    @Override
+    public Iterator getAll() {
+       if( patients.isEmpty()) return null; 
+      return  patients.values().iterator();
+        //esto regresa todas los objetos hashet        
+        
+    }
+
+    @Override
+    public int size() {
+       return patients.size();
+        
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+    return patients.isEmpty();
+    }
+        
 
     
     
--- src/waitingRoom/waitingRoomList.java
+++ src/waitingRoom/waitingRoomList.java
@@ -4,10 +4,55 @@
  */
 package waitingRoom;
 
+import clinic.SequentialDynamicLists;
+import java.util.Iterator;
+import java.util.Queue;
+import patients.Patient;
+
 /**
  *
  * @author PC
  */
-public class waitingRoomList implements SequentialDynamicLists<Patin {
+public class waitingRoomList implements SequentialDynamicLists<Patient> {
+    Queue<Patient> waitingList;
+
+    public waitingRoomList(Queue<Patient> waitingList) {
+        this.waitingList = waitingList;
+    }
+    
+    
+    
+    
+
+    @Override
+    public Patient get() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remove() {
+         return waitingList.remove(id)!= null;
+    }
+
+    @Override
+    public boolean add(Patient item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
     
 }
