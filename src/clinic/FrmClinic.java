7632fe725363a952ef80ac5c341cea3fbb864ec6
Sharys29 <sharysceciliano@gmail.com>
2026-08-20 16:43:43 -0600

    AppointmentView
    se llama e importa appointment en frmClinic

--- src/clinic/FrmClinic.java
+++ src/clinic/FrmClinic.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package clinic;
+
+import appointments.AppointmentView;
 import medicalrecords.FrmMedicalRecord;
 /**
  *
@@ -282,6 +284,11 @@ public class FrmClinic extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        AppointmentView appointmentView = new AppointmentView();
+
+        appointmentView.setLocationRelativeTo(this);
+        appointmentView.setVisible(true);
+      
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
